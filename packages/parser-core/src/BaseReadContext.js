cae1ac64de575f3afc878e6f7bbfcd5e5a7f7af1
Seong Min Park <seongmin@ridi.com>
2020-11-09 00:02:48 +0900

    feature: add base class to book,item, contexts

---
+++ packages/parser-core/src/BaseReadContext.js
@@ -0,0 +1,38 @@
+/**
+  * @typedef {Object} BaseReadOption
+  * @property {boolean} force
+  *
+  * @typedef {Object} BaseReadOptionType
+  * @property {string} force
+* /
+
+/**
+ * @class
+ */
+
+class BaseReadContext {
+  /**
+   * @pblic
+   * @type {BaseReadOption}
+   */
+  options;
+
+  /**
+   * @public
+   * @type {import('./readEntries').ReadEntriesReturnType[]}
+   */
+  entries;
+
+  /**
+   * @public
+   * @type {Array<import('./BaseBook').default>}
+   */
+  items;
+
+  constructor() {
+    this.items = undefined;
+    this.entries = undefined;
+    this.options = undefined;
+  }
+}
+export default BaseReadContext;
