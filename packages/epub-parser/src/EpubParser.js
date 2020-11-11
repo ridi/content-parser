de27303d41c17f25a93087fdcf1c8367e3617270
Seong Min Park <seongmin@ridi.com>
2020-11-11 14:34:11 +0900

    fix: handle title tag with an id

--- packages/epub-parser/src/EpubParser.js
+++ packages/epub-parser/src/EpubParser.js
@@ -326,7 +326,7 @@ class EpubParser extends Parser {
         title, creator, subject, description, publisher, contributor, date, type,
         format, identifier, source, language, relation, coverage, rights, meta,
       } = metadata;
-      rawBook.titles = getValues(title);
+      rawBook.titles = getValues(title, key => (key === textNodeName ? 'name' : key));
       rawBook.creators = getValues(creator, key => (key === textNodeName ? 'name' : key));
       rawBook.subjects = getValues(subject);
       rawBook.description = getValue(description);
