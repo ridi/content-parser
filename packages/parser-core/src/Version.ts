d7fa735028adf0760be17aec41ee607895eebbb4
Seong Min Park <seongmin@ridi.com>
2020-11-18 15:50:34 +0900

    typesscript: Version object

--- packages/parser-core/src/Version.ts
+++ packages/parser-core/src/Version.ts
@@ -1,39 +1,25 @@
 class Version {
-  /**
-   * @private
-   */
-  major;
+  private major: number;
 
-  /**
-   * @private
-   */
-  minor;
+  private minor: number;
 
-  /**
-   * @private
-   */
-  patch;
+  private patch: number;
 
-  /**
-   * Construct Version object with version.
-   * @param {string} version
-   */
-  constructor(version) {
+  constructor(version = '1.0') {
     if (!/(?:0|[1-9]\d*)(?:\.(?:0|[1-9]\d*)){0,3}/gm.test(version)) {
       version = '1.0';
     }
     const result = version.split('.');
     this.major = parseInt(result[0], 10);
-    this.minor = parseInt(result[1] || 0, 10) || 0;
-    this.patch = parseInt(result[2] || 0, 10) || 0;
+    this.minor = parseInt(result[1] || '0', 10) || 0;
+    this.patch = parseInt(result[2] || '0', 10) || 0;
     Object.freeze(this);
   }
 
   /**
    * Get Version as string
-   * @returns {string} version
    */
-  toString() {
+  toString(): string {
     return `${this.major}.${this.minor}.${this.patch}`;
   }
 }
