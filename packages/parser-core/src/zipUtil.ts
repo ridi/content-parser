8be883844427120bb5b2d1a558d2e053914a1e5e
Seong Min Park <psm03120@gmail.com>
2020-12-24 05:49:35 +0900

    migrate zupUtil to typescript

--- packages/parser-core/src/zipUtil.ts
+++ packages/parser-core/src/zipUtil.ts
@@ -5,78 +5,53 @@ import { Readable } from 'stream';
 import { trimEnd } from './bufferUtil';
 import createCryptoStream from './createCryptoStream';
 import createSliceStream from './createSliceStream';
-import CryptoProvider from './CryptoProvider';
+import CryptoProvider, { Purpose } from './CryptoProvider';
 import { isExists } from './typecheck';
 import { conditionally } from './streamUtil';
 import Errors, { createError } from './errors';
+import type Logger from './Logger';
 
-/**
- * @typedef GetFileOptions
- * @property {string} encoding
- * @property {number} end
- */
+interface GetFileOption {
+  encoding: "ascii" | "utf8" | "utf-8" | "utf16le" | "ucs2" | "ucs-2" | "base64" | "latin1" | "binary" | "hex" | undefined;
+  end: number | undefined;
+}
+export interface ZipFileInformation {
+  file: string | Buffer;
+  files: AdmZip.IZipEntry[];
+  cryptoProvider: CryptoProvider;
+  find: (entryPath: string) => AdmZip.IZipEntry | undefined,
+  getFile: (entry: AdmZip.IZipEntry, options?: GetFileOption) => Promise<Buffer | string>
+  extractAll: (unzipPath: string, overwrite?: boolean) => Promise<void>;
+  logger: Logger;
+}
 
-/**
- * @typedef {Object} ZipFileInformation
- * @property {string} file
- * @property {AdmZip.IZipEntry[]} files
- * @property {CryptoProvider} cryptoProvider
- * @property {(entryPath: string) => AdmZip.IZipEntry} find
- * @property {(entry: AdmZip.IZipEntry, options?: GetFileOptions) => Promise<Buffer | String>} getFile
- * @property {(unzipPath: string, overwrite?: boolean) => Promise<any>} extractAll
- * @property {import('./Logger').default} logger
- */
-
-/**
- * Find the file with a path.
- * @param {string} entryPath File Path
- * @returns {AdmZip.IZipEntry | undefined} A file with path or undefined if there is none.
- * */
-function find(entryPath) {
+function find(this: ZipFileInformation, entryPath: string): AdmZip.IZipEntry | undefined {
   return this.files.find(entry => entryPath === entry.entryName);
 }
 
-/**
- *
- * @async
- * @this {ZipFileInformation}
- * @param {import('adm-zip').IZipEntry} entry
- * @param {GetFileOptions} options
- * @returns {Promise<Buffer | String>} String is encoding is provided, Buffer otherwise
- */
-async function getFile(entry, options = { encoding: undefined, end: undefined }) {
+async function getFile(this: ZipFileInformation, entry: AdmZip.IZipEntry, options: GetFileOption = { encoding: undefined, end: undefined }) {
   const { encoding, end } = options;
-  let file = await new Promise((resolveFile) => {
-    const totalSize = Math.min(end || Infinity, entry.uncompressedSize);
+  let file = await new Promise<Buffer>((resolveFile) => {
     let data = Buffer.from([]);
     const readable = Readable.from(entry.getData());
+    const bindedConditionally = conditionally.bind(readable);
     readable
-      .pipe(conditionally(isExists(end), createSliceStream(0, end)))
-      .pipe(conditionally(this.cryptoProvider && !!this.cryptoProvider.isStreamMode,
-        createCryptoStream(entry.path, totalSize, this.cryptoProvider, CryptoProvider.Purpose.READ_IN_DIR)))
-      .on('data', (chunk) => { data = Buffer.concat([data, chunk]); })
+      .pipe(bindedConditionally(isExists(end), createSliceStream(0, end)))
+      .pipe(bindedConditionally(this.cryptoProvider && !!this.cryptoProvider.isStreamMode,
+        createCryptoStream(entry.entryName, Infinity, this.cryptoProvider, Purpose.READ_IN_DIR)))
+      .on('data', (chunk: Uint8Array) => { data = Buffer.concat([data, chunk]); })
       .on('end', () => { resolveFile(data); });
   });
   if (this.cryptoProvider && !this.cryptoProvider.isStreamMode) {
-    file = this.cryptoProvider.run(file, entry.path, CryptoProvider.Purpose.READ_IN_DIR);
-    if (Promise.resolve(file) === file) {
-      file = await file;
-    }
+    file = await this.cryptoProvider.run(file, entry.entryName, Purpose.READ_IN_DIR);
   }
   if (isExists(encoding)) {
-    file = trimEnd(file).toString(encoding);
+    return trimEnd(file).toString(encoding);
   }
   return file;
 }
 
-/**
- * Extract zip file to path
- * @this {ZipFileInformation}
- * @param {string} unzipPath Path where files will be extracted
- * @param {boolean} overwrite
- * @returns {Promise<void>}
- */
-async function extractAll(unzipPath, overwrite = true) {
+async function extractAll(this: ZipFileInformation, unzipPath: string, overwrite = true): Promise<void> {
   if (overwrite) {
     fs.removeSync(unzipPath);
   }
@@ -84,24 +59,18 @@ async function extractAll(unzipPath, overwrite = true) {
   const zip = new AdmZip();
   await Promise.all(this.files.map(async (entry) => {
     if (this.cryptoProvider && !entry.isDirectory) {
-      entry.setData(await this.cryptoProvider.run(entry.getData(), entry.entryPath, CryptoProvider.Purpose.WRITE));
+      entry.setData(await this.cryptoProvider.run(entry.getData(), entry.entryName, Purpose.WRITE));
     }
     zip.addFile(entry.entryName, entry.getData());
   }));
   zip.extractAllTo(unzipPath);
 }
 
-/**
- * @param {string | Buffer} file
- * @param {CryptoProvider} cryptoProvider
- * @param {Logger} logger
- * @returns {ZipFileInformation}
- * @throws {Errors.ENOENT} When file can't be found
- */
-export default async function openZip(file, cryptoProvider, logger) {
+export default async function openZip(file: string | Buffer, cryptoProvider: CryptoProvider, logger: Logger): Promise<ZipFileInformation> {
   try {
     const files = new AdmZip(file).getEntries();
     return {
+      file,
       files,
       cryptoProvider,
       find,
@@ -110,6 +79,6 @@ export default async function openZip(file, cryptoProvider, logger) {
       logger,
     };
   } catch (err) {
-    throw createError(Errors.ENOENT, file);
+    throw createError(Errors.ENOENT, file.toString());
   }
 }
