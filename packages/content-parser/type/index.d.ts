93d39155bd2e06ae1b1d1fb9e1f75628b7c7eba3
Y.Y <lowfronterior@gmail.com>
2023-04-21 13:57:39 +0900

    기존 타입 제거

--- packages/content-parser/type/index.d.ts
+++ packages/content-parser/type/index.d.ts
@@ -1,12 +1,5 @@
 import { EpubParser } from "@ridi/epub-parser";
 import { EpubBook } from "@ridi/epub-parser";
-import { ComicParser } from "@ridi/comic-parser";
-import { ComicBook } from "@ridi/comic-parser";
 import { PdfParser } from "@ridi/pdf-parser";
 import { PdfBook } from "@ridi/pdf-parser";
-import { Errors } from "@ridi/parser-core";
-import { LogLevel } from "@ridi/parser-core";
-import { CryptoProvider } from "@ridi/parser-core";
-import { AesCryptor } from "@ridi/parser-core";
-import { Hash } from "@ridi/parser-core";
 export { EpubParser, EpubBook, ComicParser, ComicBook, PdfParser, PdfBook, Errors, LogLevel, CryptoProvider, AesCryptor, Hash };
--- packages/parser-core/type/parseBool.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Return boolean form of any input
- * @param {any} any
- * @returns {boolean}
- */
-export default function parseBool(any: any): boolean;
